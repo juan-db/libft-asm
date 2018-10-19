8fa69bbe951f1e36af1756cc014c82964249d28b
Juan de Bruin <27200735+juan-db@users.noreply.github.com>
2018-10-19 09:57:43 +0200

    Make ft_isascii pass tests

--- source/ft_isascii.s
+++ source/ft_isascii.s
@@ -3,14 +3,14 @@ section .text
 
 ; characters 0 to 0177
 _ft_isascii:
-; 	cmp rdi, 0
-; 	jl not_equal
-; 	cmp rdi, 177o
-; 	jg not_equal
+	cmp rdi, 0
+	jl not_equal
+	cmp rdi, 177o
+	jg not_equal
 
-; equal:
-; 	mov rax, 1		; move 1 into %rax, 1st return register
-; 	ret
+equal:
+	mov rax, 1		; move 1 into %rax, 1st return register
+	ret
 
 not_equal:
 	mov rax, 0		; move 0 into %rax, 1st return register
